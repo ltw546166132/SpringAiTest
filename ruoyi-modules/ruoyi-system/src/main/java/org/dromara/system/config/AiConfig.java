a921f65351bbd059f843bf36489853eb8dc6f0f0
QEES <fariyajahan303@gmail.com>
2026-02-05 00:36:55 +0800

    feat(ai): 集成阿里云AI和更新聊天模型配置
    - 引入阿里云AI相关依赖spring-ai-alibaba-graph-core
    - 在根pom.xml中添加阿里云AI版本和BOM依赖管理
    - 修改OpenAi ChatClient默认系统消息，增加当前北京时间显示
    - 将开发环境chat模型由gpt-4o-mini切换为gemini-3-flash-preview

--- ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/config/AiConfig.java
+++ ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/config/AiConfig.java
@@ -1,6 +1,7 @@
 package org.dromara.system.config;
 
 import jakarta.annotation.Resource;
+import org.dromara.common.core.utils.DateUtils;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SafeGuardAdvisor;
@@ -41,7 +42,7 @@ public class AiConfig {
 
     @Bean
     public ChatClient openAiChatClient(ChatModel openAiChatModel) {
-        return ChatClient.builder(openAiChatModel).defaultAdvisors(MessageChatMemoryAdvisor.builder(mySqlChatMemory()).build(), SafeGuardAdvisor.builder().sensitiveWords(List.of("敏感词")).build()).build();
+        return ChatClient.builder(openAiChatModel).defaultSystem("当前北京时间是"+ DateUtils.getTime()).defaultAdvisors(MessageChatMemoryAdvisor.builder(mySqlChatMemory()).build(), SafeGuardAdvisor.builder().sensitiveWords(List.of("敏感词")).build()).build();
     }
 
 
